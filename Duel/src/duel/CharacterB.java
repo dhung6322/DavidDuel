3d6a1327b765294e244da9d4a20a544d28dd4b19
Ethan <etam4225@bths.edu>
2017-09-14 14:51:58 -0400

    added chara and methods

--- Duel/src/duel/CharacterB.java
+++ Duel/src/duel/CharacterB.java
@@ -1,5 +1,17 @@
 package duel;
 
-public class CharacterB {
+public class CharacterB implements Dueler{
 
+	public CharacterB() {
+		
+	}
+	public void taunt() {
+		System.out.print.ln("Come at me");
+	}
+	public String getName{
+		
+	}
+	public void setStartingHP(int hp) {
+		hp = 100;
+	}
 }
