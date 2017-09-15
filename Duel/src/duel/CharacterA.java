fe53406032483182a76fea24cab6cf4a50e375ba
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-09-15 15:34:01 -0400

    Created Character A

---
+++ Duel/src/duel/CharacterA.java
@@ -0,0 +1,30 @@
+package duel;
+
+public class CharacterA implements Dueler {
+
+	public void taunt() {
+		
+	}
+
+
+	public String getName() {
+		return null;
+	}
+
+	public void setStartingHP(int hp) {
+
+		
+	}
+
+
+	public int getHP() {
+		return 0;
+	}
+
+
+	public boolean determineIfOpponentIsFair(Dueler d, int target) {
+
+		return false;
+	}
+
+}
